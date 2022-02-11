ee92a1fcfd1d6f24b17b8aaaf793113892b163d8
Julia <kobernikulia@gmail.com>
2022-02-11 17:37:42 +0200

    modified all files

--- coursework_JS_advanced/index.js
+++ coursework_JS_advanced/index.js
@@ -1,30 +1,8 @@
-let city = document.querySelectorAll('.city-name');
-/*let london = document.querySelector('.london-name');
-let ny = document.querySelector('.ny-name');*/
-
-let cityTemp = document.querySelectorAll('.city-temp');
-/*let londonFeatures = document.querySelector('.london-features');
-let nyFeatures = document.querySelector('.ny-features');*/
-
-let feelsLike = document.querySelectorAll('.feels-like');
-
-let btnKiev = document.querySelector('.btn1');
-let btnLondon = document.querySelector('.btn2');
-let btnNY = document.querySelector('.btn3');
-
-let urlKiev = 'http://api.openweathermap.org/data/2.5/weather?id=703448&appid=bf35cac91880cb98375230fb443a116f';
-let urlLondon = 'http://api.openweathermap.org/data/2.5/weather?id=2643743&appid=bf35cac91880cb98375230fb443a116f';
-let urlNY = 'http://api.openweathermap.org/data/2.5/weather?id=5128638&appid=bf35cac91880cb98375230fb443a116f';
-
-btnKiev.addEventListener('click', function() {
-    getData(urlKiev);
-});
-btnLondon.addEventListener('click', function() {
-    getData(urlLondon);
-});
-btnNY.addEventListener('click', function() {
-    getData(urlNY);
-});
+let city = document.querySelector('.city-name');
+let cityTemp = document.querySelector('.city-temp');
+let feelsLike = document.querySelector('.feels-like');
+let btn = document.querySelector('.btn');
+let apiKey = 'bf35cac91880cb98375230fb443a116f';
 
 async function getData(url) {
 
@@ -33,25 +11,35 @@ async function getData(url) {
         console.log(e.message)
     });
     const data  = await response.json();
-    if(url == urlKiev)
-        {
-            city[0].textContent = data.name;
-            cityTemp[0].innerHTML = 'TEMPERATURE: ' + Math.round(data.main.temp-273) + '&deg';
-            feelsLike[0].innerHTML = 'FEELS LIKE: ' + Math.round(data.main.feels_like-273) + '&deg'; 
-        }
-        else if (url == urlLondon)
-        {
-            city[1].textContent = data.name;
-            cityTemp[1].innerHTML = 'TEMPERATURE: ' + Math.round(data.main.temp-273) + '&deg';
-            feelsLike[1].innerHTML = 'FEELS LIKE: ' + Math.round(data.main.feels_like-273) + '&deg'; 
-        }
-        else if(url == urlNY)
-        {
-            city[2].textContent = data.name;
-            cityTemp[2].innerHTML = 'TEMPERATURE: ' + Math.round(data.main.temp-273) + '&deg';
-            feelsLike[2].innerHTML = 'FEELS LIKE: ' + Math.round(data.main.feels_like-273) + '&deg'; 
-        }       
+    console.log(data);
+    return data;  
 };
+
+class showInfo {
+    constructor(info, city, cityTemp, feelsLike) {
+        this.info = info;
+        this.city = city;
+        this.cityTemp = cityTemp;
+        this.feelsLike = feelsLike;
+    }
+    show() {
+        this.city.textContent = this.info.name;
+        this.cityTemp.innerHTML = 'TEMPERATURE: ' + Math.round(this.info.main.temp-273) + '&deg';
+        this.feelsLike.innerHTML = 'FEELS LIKE: ' + Math.round(this.info.main.feels_like-273) + '&deg';
+    }
+}
+
+btn.addEventListener('click', async function(e) {
+    e.preventDefault();
+    let form = document.forms[0];
+    let input = document.getElementById("inputCity").value;
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${apiKey}`
+    //getData(url);
+    let dataInfo = await getData(url);
+    let infoChild = new showInfo(dataInfo, city, cityTemp, feelsLike);
+    infoChild.show(); 
+});
+
 
 
 
